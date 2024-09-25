79256b2f1c740456b348f11d8bebfdc0eac9940c
libing <libing@healink.cn>
2024-09-25 17:21:36 +0800

    MOD: support create dataset

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 import asyncio
 
+from dify_eval.dataset import dataset
 from dify_eval.eval import langfuse_eval
 
 if __name__ == "__main__":
+    # upload local file to langfuse dataset
+    dataset.create_dataset_from_file()
+
+    # run dify call by dataset
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     results = loop.run_until_complete(langfuse_eval.run_dataset_and_save())
-    print(results)
