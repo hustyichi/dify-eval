66f4568a438357fe5bd88fe7fb4cda55f7f0f818
libing <libing@healink.cn>
2024-09-25 14:00:37 +0800

    ADD: add eval package

---
+++ dify_eval/eval/dify_chat.py
@@ -0,0 +1,45 @@
+import os
+from typing import Literal
+
+import aiohttp
+from dotenv import load_dotenv
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+load_dotenv()
+
+
+@retry(stop=stop_after_attempt(10), wait=wait_fixed(3))
+async def send_chat_message(
+    query: str,
+    inputs: dict = {},
+    url: str = os.getenv("DIFY_API_BASE", ""),
+    api_key: str = os.getenv("DIFY_API_KEY", ""),
+    response_mode: Literal["streaming", "blocking"] = "blocking",
+    user: str = "auto_test",
+    file_array: list = [],
+):
+    if not (url and api_key):
+        raise ValueError(
+            "DIFY_API_BASE and DIFY_API_KEY must be set in the environment variables"
+        )
+
+    chat_url = f"{url}/chat-messages"
+    headers = {"Authorization": f"Bearer {api_key}", "Content-Type": "application/json"}
+    payload = {
+        "inputs": inputs,
+        "query": query,
+        "response_mode": response_mode,
+        "conversation_id": "",
+        "user": user,
+        "files": file_array,
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(chat_url, headers=headers, json=payload) as response:
+            ret = await response.json()
+            status = ret.get("status")
+            message = ret.get("message")
+            if status and message:
+                raise ValueError(f"{status}: {message}")
+            if ret.get("answer") == "":
+                raise ValueError("Empty answer")
+            return ret
---
+++ dify_eval/eval/langfuse_eval.py
@@ -0,0 +1,77 @@
+import asyncio
+import os
+from datetime import datetime
+
+import pandas as pd
+from dotenv import load_dotenv
+from eval.dify_chat import send_chat_message
+from langfuse import Langfuse
+
+load_dotenv()
+
+langfuse = Langfuse()
+
+
+def get_langfuse_dataset(dataset_name: str):
+    return langfuse.get_dataset(dataset_name)
+
+
+async def run_dataset_item(item, run_name, semaphore):
+    async with semaphore:
+        response = await send_chat_message(item.input)
+
+        item.link(
+            trace_or_observation=None,
+            run_name=run_name,
+            trace_id=response["message_id"],
+            observation_id=None,
+        )
+        return response
+
+
+def save_results(results: list[dict], local_path: str = ""):
+    data = []
+    for result in results:
+        data.append(result["answer"].strip())
+
+    df = pd.DataFrame(data, columns=["answer"])
+
+    if not local_path:
+        current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+        local_path = f'submit/results_{os.getenv("RUN_NAME", "")}_{current_time}.csv'
+
+    df.to_csv(local_path, index=False)
+
+
+async def run_dataset(
+    dataset_name: str = os.getenv("DATASET_NAME", ""),
+    run_name: str = os.getenv("RUN_NAME", ""),
+    max_concurrency: int = 3,
+):
+
+    if not dataset_name:
+        raise ValueError("No dataset name provided.")
+
+    if not run_name:
+        run_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    dataset = get_langfuse_dataset(dataset_name)
+    semaphore = asyncio.Semaphore(max_concurrency)
+    tasks = []
+    input_data = []
+    for item in dataset.items:
+        task = asyncio.create_task(run_dataset_item(item, run_name, semaphore))
+        input_data.append(item.input)
+        tasks.append(task)
+
+    results = await asyncio.gather(*tasks)
+    return results
+
+
+async def run_dataset_and_save(
+    dataset_name: str = os.getenv("DATASET_NAME", ""),
+    run_name: str = os.getenv("RUN_NAME", ""),
+    max_concurrency: int = 1,
+):
+    results = await run_dataset(dataset_name, run_name, max_concurrency)
+    save_results(results)
