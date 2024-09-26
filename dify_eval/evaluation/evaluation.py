e1bec8bc39a605949d72b6b8c026831e03f35dee
libing <libing@healink.cn>
2024-09-26 14:05:05 +0800

    ADD: support get traces

--- dify_eval/evaluation/evaluation.py
+++ dify_eval/evaluation/evaluation.py
@@ -2,11 +2,18 @@ import os
 
 from dotenv import load_dotenv
 from langfuse import Langfuse
+from langfuse.client import FetchTracesResponse
+
+from dify_eval.evaluation import constants
 
 load_dotenv()
 
 langfuse = Langfuse()
 
 
-def get_run_traces(user_id: str = os.getenv("RUN_NAME", "auto_test_user")):
-    return langfuse.fetch_traces(user_id=user_id)
+def get_run_traces(
+    user_id: str = os.getenv("RUN_NAME", "auto_test_user"),
+    page: int = 0,
+    limit: int = constants.BATCH_SIZE,
+) -> FetchTracesResponse:
+    return langfuse.fetch_traces(user_id=user_id, page=page, limit=limit)
